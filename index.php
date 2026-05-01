b2466f23194dd0b80ddb32a44a3fa33cc10bb99b
John leo <ummercool559@gmail.com>
2026-05-01 14:20:07 +0800

    created the welcome page, using css animations and styles

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <link rel="stylesheet" href="./assets/css/welcome-styles.css">
+ <title>MoveOn</title>
+</head>
+
+<body>
+
+ <main>
+  <div id="container">
+   <h2>Welcome to</h2>
+   <div class="logo-box">
+    <h1 class="logo">MoveOn</h1>
+   </div>
+   <p class="text-right">A place where you can vent, relapse, read inspiring stories and track your healing progress.</p>
+   <button class="continue">Continue </button>
+  </div>
+ </main>
+
+ <footer>
+  <h4>Copyright &copy; 2026 All Rights Reserved</h4>
+  <h4>Made with &#x2764; by <a href="#container" target="_blank">@johnleojariel</a></h4>
+ </footer>
+
+</body>
+
+</html>
---
+++ onboarding.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>Onboarding | Question <?= 1 ?></title>
+</head>
+
+<body>
+
+ <main>
+  <div id="container">
+   <h1 class="header-1">Hi! You are exactly where you are. It's not a mistake.</h1>
+   <h2 class="header-2">What brought you here?</h2>
+  </div>
+ </main>
+
+</body>
+
+</html>
