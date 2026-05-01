1542415746add80d4ee67a017f1e7698b910f19c
John leo <ummercool559@gmail.com>
2026-05-01 16:14:33 +0800

    done designing the question 1

---
+++ includes/onboard-q/question-1.php
@@ -0,0 +1,6 @@
+<label for="question-1">A recent breakup or loss<input type="radio" id="question-1" name="oboarding-q" value="1" required checked/></label>
+<label for="question-2">A major life change or setback<input type="radio" id="question-2" name="oboarding-q" value="2" required/></label>
+<label for="question-3">Feeling "stuck" in the past<input type="radio" id="question-3" name="oboarding-q" value="3" required/></label>
+<label for="question-4">A friend / loved one recommended this<input type="radio" id="question-4" name="oboarding-q" value="4" required/></label>
+<label for="question-5">I saw a post that resonated me<input type="radio" id="question-5" name="oboarding-q" value="5" required/></label>
+<label for="question-6">Other / I'd rather not say<input type="radio" id="question-6" name="oboarding-q" value="6" required/></label>
--- onboarding.php
+++ onboarding.php
@@ -4,15 +4,33 @@
 <head>
  <meta charset="UTF-8">
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <link rel="stylesheet" href="./assets/css/onboarding-styles.css">
  <title>Onboarding | Question <?= 1 ?></title>
 </head>
 
 <body>
 
  <main>
-  <div id="container">
+  <div id="main-container">
    <h1 class="header-1">Hi! You are exactly where you are. It's not a mistake.</h1>
    <h2 class="header-2">What brought you here?</h2>
+
+   <form action="">
+    <div id="sub-container">
+     <label for="question-1">A recent breakup or loss<input type="radio" id="question-1" name="oboarding-q" value="1" required checked/></label>
+     <label for="question-2">A major life change or setback<input type="radio" id="question-2" name="oboarding-q" value="2" required/></label>
+     <label for="question-3">Feeling "stuck" in the past<input type="radio" id="question-3" name="oboarding-q" value="3" required/></label>
+     <label for="question-4">A friend / loved one recommended this<input type="radio" id="question-4" name="oboarding-q" value="4" required/></label>
+     <label for="question-5">I saw a post that resonated me<input type="radio" id="question-5" name="oboarding-q" value="5" required/></label>
+     <label for="question-6">Other / I'd rather not say<input type="radio" id="question-6" name="oboarding-q" value="6" required/></label>
+
+     <div id="buttons-q-nav">
+      <button class="previous-btn">&#8592; Back</button>
+      <button class="skip-btn">Skip</button>
+      <button type="submit" class="next-btn">Next &#8594;</button>
+     </div>
+    </div>
+   </form>
   </div>
  </main>
 
